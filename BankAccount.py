c1668dcec00ad5ac9782a5a49a2715538dddc0cc
Mohammad Yasin <mohammad.yasin@students.makeschool.com>
2020-11-09 22:35:12 -0800

    creating receipt for the accounts' owner.

--- BankAccount.py
+++ BankAccount.py
@@ -13,23 +13,35 @@ class BankAccount():
 
     def deposit(self, amount):
         self.balance += amount
-        return self.balance
+        print (f'Amount Deposited: ${amount}')
         
     def withdraw(self, amount):
         if self.balance >= amount:
             self.balance -= amount
-            # print("Amount Withdrawn:")
+            print(f'Amount Withdrawn: ${amount}')
         else:
             print("Insufficient funds")
 
     def get_balance(self):
         return f'Your current balance is: ${self.balance}'
 
-    # def add_interest(self):
-        # interest = self.balance *  0.00083
+    def add_interest(self):
+        monthly_interest = self.balance *  0.00083
+        print(f'Your monthly interest is: {monthly_interest}')
 
-account = BankAccount("Mohammad", 2017264, 772017264, 2600)
+    def print_receipt(self):
+        print()
+        # print(self.full_name)
+        # print(self.account_number)
+        # print(self.routing_number)
+        # print(self.balance)
+        print('{} with account# {} has ${} in account.'.format(self.full_name, self.account_number, self.balance))
+        
+
+account = BankAccount("Mohammad", 20172648, 772017264, 2600)
 account.deposit(900)
 # print(account.get_balance())
 account.withdraw(220)
 print(account.get_balance())
+account.add_interest()
+account.print_receipt()
